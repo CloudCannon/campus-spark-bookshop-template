f46854d1b2edf5245dee6547e522de0ab12e09f2
Mike Neumegen <mneumegen@gmail.com>
2024-04-27 12:46:34 -0700

    mobile menu working

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,15 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-	Array.from(document.querySelectorAll('.dropdown button')).forEach(function (dropdownButton) {
-		dropdownButton.addEventListener('click', function (e) {
-			// Toggle the 'is-active' class on the dropdown
-			dropdownButton.parentNode.classList.toggle('is-active');
-		
-			// Optionally, remove 'is-active' from all other dropdowns if needed
-			document.querySelectorAll('.dropdown.is-active').forEach(function (active) {
-				if (active !== dropdownButton.parentNode) {
-					active.classList.remove('is-active');
-				}
-			});
-		});
-	});
-}, false);
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    const dropdownButtons = document.querySelectorAll('.dropdown > button');
+
+    function toggleDropdown() {
+        const currentButton = this;
+        const currentDropdown = currentButton.parentNode;
+        const isExpanded = currentButton.getAttribute('aria-expanded') === 'true';
+		const parentDropdown = currentDropdown.parentNode.closest('.dropdown');
+
+		closeDropdowns(parentDropdown);
+
+        if (!isExpanded) {
+            currentButton.setAttribute('aria-expanded', 'true');
+            currentDropdown.classList.add('is-active');
+        }
+    }
+
+    function closeDropdowns(element) {
+		let buttons = dropdownButtons;
+
+		if (element) {
+			buttons = element.querySelectorAll('& .dropdown > button');
+		}
+
+        buttons.forEach(button => {
+            button.setAttribute('aria-expanded', 'false');
+            button.parentNode.classList.remove('is-active');
+        });
+    }
+
+    dropdowns.forEach(dropdown => {
+        dropdown.classList.add('dropdown-js');
+    });
+
+    dropdownButtons.forEach(button => {
+        button.addEventListener('click', toggleDropdown);
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === "Escape") {
+            closeDropdowns();
+        }
+    });
+});
