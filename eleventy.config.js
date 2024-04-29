35afffa581df57b8ffebddcb0754d082cb6aac4b
Mike Neumegen <mneumegen@gmail.com>
2024-04-29 16:57:05 -0700

    bookshopped

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,44 @@
 const svgContents = require("eleventy-plugin-svg-contents");
+const Image = require("@11ty/eleventy-img");
+const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addWatchTarget("./src/assets/style.css");
 	eleventyConfig.addPassthroughCopy("./src/assets/");
 	eleventyConfig.addPlugin(svgContents);
 
+	eleventyConfig.addShortcode("image", async function (src, alt, widths, sizes) {
+		if (src === undefined) {
+			return "";
+		}
+
+		let metadata = await Image(`src/${src}`, {
+			widths: [widths],
+			formats: ["avif", "webp", "svg", "jpeg"],
+			outputDir: "./_site/optimized/",
+      		urlPath: "/optimized/",
+		});
+
+		let imageAttributes = {
+			alt,
+			sizes,
+			loading: "lazy",
+			decoding: "async",
+		};
+
+		return Image.generateHTML(metadata, imageAttributes);
+	});
+
+	eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
+
+	eleventyConfig.addPlugin(pluginBookshop({
+		bookshopLocations: ["src/_component-library"],  
+		pathPrefix: '', 
+	  }));
+
 	return {
-		dir: {
+		dir: {		
 			input: "src",
 			output: "_site"
 		}
--- src/assets/main.js
+++ src/assets/main.js
@@ -1,44 +1,52 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const dropdowns = document.querySelectorAll('.dropdown');
-    const dropdownButtons = document.querySelectorAll('.dropdown > button');
+	const dropdowns = document.querySelectorAll('.dropdown');
+	const dropdownButtons = document.querySelectorAll('.dropdown > button');
 
-    function toggleDropdown() {
-        const currentButton = this;
-        const currentDropdown = currentButton.parentNode;
-        const isExpanded = currentButton.getAttribute('aria-expanded') === 'true';
+	function toggleDropdown() {
+		const currentButton = this;
+		const currentDropdown = currentButton.parentNode;
+		const isExpanded = currentButton.getAttribute('aria-expanded') === 'true';
 		const parentDropdown = currentDropdown.parentNode.closest('.dropdown');
 
 		closeDropdowns(parentDropdown);
 
-        if (!isExpanded) {
-            currentButton.setAttribute('aria-expanded', 'true');
-            currentDropdown.classList.add('is-active');
-        }
-    }
+		if (!isExpanded) {
+			currentButton.setAttribute('aria-expanded', 'true');
+			currentDropdown.classList.add('is-active');
+		}
+	}
 
-    function closeDropdowns(element) {
+	function closeDropdowns(element) {
 		let buttons = dropdownButtons;
 
 		if (element) {
 			buttons = element.querySelectorAll('& .dropdown > button');
 		}
 
-        buttons.forEach(button => {
-            button.setAttribute('aria-expanded', 'false');
-            button.parentNode.classList.remove('is-active');
-        });
-    }
+		buttons.forEach(button => {
+			button.setAttribute('aria-expanded', 'false');
+			button.parentNode.classList.remove('is-active');
+		});
+	}
 
-    dropdowns.forEach(dropdown => {
-        dropdown.classList.add('dropdown-js');
-    });
+	dropdowns.forEach(dropdown => {
+		dropdown.classList.add('dropdown-js');
+	});
 
-    dropdownButtons.forEach(button => {
-        button.addEventListener('click', toggleDropdown);
-    });
+	dropdownButtons.forEach(button => {
+		button.addEventListener('click', toggleDropdown);
+	});
 
-    document.addEventListener('keydown', function(event) {
-        if (event.key === "Escape") {
+	document.addEventListener('keydown', function(event) {
+		if (event.key === "Escape") {
+			closeDropdowns();
+		}
+	});
+
+	document.addEventListener('click', function(event) {
+        const isClickInside = event.target.closest('.dropdown');
+
+        if (!isClickInside) {
             closeDropdowns();
         }
     });
