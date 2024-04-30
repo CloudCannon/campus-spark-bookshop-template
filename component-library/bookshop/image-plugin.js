a03c0edf4fac3019ee0ff2df97fdf9017b5af76e
Mike Neumegen <mneumegen@gmail.com>
2024-04-30 12:11:41 -0700

    added branding

--- component-library/bookshop/image-plugin.js
+++ component-library/bookshop/image-plugin.js
@@ -1,5 +1,15 @@
 module.exports = function (Liquid) {
-    this.registerTag('image', (src, alt, width) => {
-      return `<picture><img src="${src}" alt="${alt}" width="${width}" /></picture>`;
-    });
+    this.registerTag('image', {
+      parse: function(token, remainingTokens) {
+        this.src = token.args.split(' ')[0];
+      },
+      render: function(scope, hash) {
+        return `<img src="${this.src}" alt="" width="" />`;
+      }
+    }
+    
+    
+    // (src, alt, widths, sizes) => {
+    //   return `<picture><img src="${src}" alt="${alt}" width="${width}" /></picture>`;
+    // });
 }
